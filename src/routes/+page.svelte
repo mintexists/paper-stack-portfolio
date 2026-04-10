240f29b42731422a8dfd095f2cc203592228082b
mint <60362123+mintexists@users.noreply.github.com>
2026-04-10 18:05:06 -0400

    aa

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { setContext } from 'svelte';
+	import PaperContainer from './PaperContainer.svelte';
+
+	import { motion } from '@humanspeak/svelte-motion';
+
+	let containerEl: HTMLElement;
+
+	setContext('containerEl', () => containerEl);
+
+	// $inspect($scrollYProgress);
+</script>
+
+<div
+	class="h-screen w-full snap-y snap-mandatory overflow-x-clip overflow-y-auto"
+	bind:this={containerEl}
+>
+	{#each new Array(5).fill(0) as _, i (i)}
+		<PaperContainer>
+			<motion.div
+				drag
+				dragMomentum={false}
+				class="pointer-events-auto relative h-[90%] w-1/3 bg-red-400 shadow-xl hover:scale-101"
+				onwheel={(e: WheelEvent & { currentTarget: EventTarget & HTMLDivElement }) => {
+					e.preventDefault();
+					// Object.defineProperty(e, 'target', { writable: false, value: containerEl });
+					// containerEl.dispatchEvent(e);
+					containerEl.scrollBy(0, e.deltaY * 0.00001);
+					console.log(e);
+					// const delta = e.deltaY;
+					// // const maxScroll = containerEl.scrollHeight - containerEl.clientHeight;
+					// containerEl.scrollTop = containerEl.scrollTop + delta;
+				}}
+			></motion.div>
+		</PaperContainer>
+	{/each}
+	<div class="h-screen"></div>
+</div>
+
+<style>
+	:global(body) {
+		overflow: clip;
+	}
+</style>
